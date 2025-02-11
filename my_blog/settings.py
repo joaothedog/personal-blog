7e17de90f02f3922cdad3dc5ba4009ff6f78e991
Dog <jvcbatist4@outlook.com>
2025-02-10 21:01:00 -0300

    Setup apps

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
